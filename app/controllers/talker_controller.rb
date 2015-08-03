c2a2a1b45c3f9f4d5e586df63655efeae0a4a95a
임광국 <kwangkuk0821@gmail.com>
2015-08-03 19:15:08 +0900

    talker 컨트롤러 create 액션 작성
    * talker 컨트롤러에서 create 기능 구현하기.
    * ask 값을 받아서 이를 이용해서 Talk 레코드들을 찾음
    * 찾은 Talk 레코드를 이용해서 Chat 레코드(대화 내역)를 생성함

--- app/controllers/talker_controller.rb
+++ app/controllers/talker_controller.rb
@@ -1,4 +1,17 @@
 class TalkerController < ApplicationController
   def index
   end
+
+  def create
+    talk = Talk.where(ask: params[:ask]).sample
+
+    unless talk.nil?
+      Chat.create(chat_type: "user", chat_content: talk.ask)
+      Chat.create(chat_type: "bot", chat_content: talk.answer)
+      redirect_to '/'
+    else
+      redirect_to '/learn'
+    end
+
+  end
 end
