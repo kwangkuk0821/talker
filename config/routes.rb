e039dc02e92ad6c12b0a768ee38771ba4044f07d
임광국 <kwangkuk0821@gmail.com>
2015-08-03 15:29:44 +0900

    root 경로 설정, resources 옵션 except
    * resources :talker 를 통해 생성되는 경로 중 index 를 제외시킨다.
    * root('/') 를 talker 컨트롤러의 index 액션과 연결시킨다.
    * 그렇게하면 talker 컨트롤러의 index 액션만 '/' 경로로 접근할 수 있다.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :talker
+  root 'talker#index'
+  resources :talker, except: :index
   resources :learn
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
