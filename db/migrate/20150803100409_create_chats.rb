9419833e9b65d943bafbfb78963707626242c663
임광국 <kwangkuk0821@gmail.com>
2015-08-03 19:06:12 +0900

    Chat 마이그레이션 파일 작성
    * chat_type - 누가 말한 것인지(User or Bot), chat_content - 대화 내용

--- db/migrate/20150803100409_create_chats.rb
+++ db/migrate/20150803100409_create_chats.rb
@@ -1,6 +1,8 @@
 class CreateChats < ActiveRecord::Migration
   def change
     create_table :chats do |t|
+      t.string :chat_type
+      t.text :chat_content
 
       t.timestamps null: false
     end
